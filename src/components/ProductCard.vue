c82dd7e294114996c7077515a3c1b33397021076
yunndian27 <71434288+yunndian27@users.noreply.github.com>
2021-07-20 22:02:44 +0800

    upload

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+b-card.product-catd.mb-2(
+  :title="product.name"
+  :sub-title="'$'+product.price"
+  img-top
+  :img-src="product.image"
+)
+  b-card-text(style="white-space: pre-line;") {{ product.description }}
+</template>
+
+<script>
+export default {
+  name: 'ProductCard',
+  props: {
+    product: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
